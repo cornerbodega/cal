a5b6f26968ad137580e2d2eac7e02b18055ed2d4
Cornerbodega <merhone@gmail.com>
2016-01-06 18:12:02 -0800

    where do I go?

--- sockets/index.js
+++ sockets/index.js
@@ -4,23 +4,22 @@ var io = require('socket.io')(http);
 
 
 app.get('/', function(req, res){
-  res.sendFile('/var/www/potnet.net/index.html');
+    res.sendFile('/var/www/potnet.net/index.html');
 });
 
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
 
-//io.on('connection', function(socket){
-//  console.log('a user connected');
-//  socket.on('disconnect', function(){
-//    console.log('user disconnected');
-//  });
-//});
 io.on('connection', function(socket){
-socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-  });
-}
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+    });
 });
