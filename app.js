46cafdf97a07740844e5c9d1039a52e3cf5db62c
Cornerbodega <merhone@gmail.com>
2016-01-07 12:34:37 -0800

    where do I go?

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ var proxy = require('express-http-proxy');
 var app = express();
 var http = require('http')
 var port = process.env.PORT || 8080;
-var wslcb_server = 'https://wslcb.mjtraceability.com/serverjson.asp'
+var wslcb_server = 'https://wslcb.mjtraceability.com'
+var login_req = {
+    "API": "4.0",
+    "action": "login",
+    "password":"44Million!",
+    "license_number":603347225,
+    "username":"luchinisupercritical@gmail.com"
+}
 // app.use('/proxy', proxy('https://wslcb.mjtraceability.com/serverjson.asp', {
 //   forwardPath: function(req, res) {
 //     return require('url').parse(req.url).path;
@@ -12,10 +19,45 @@ var wslcb_server = 'https://wslcb.mjtraceability.com/serverjson.asp'
 app.post('/login', function (req, res, next) {
   // res.sendFile('/var/www/potnet.net/index.html')
   // console.log(req);
-  http.post(wslcb_server, req).success(function(res){console.log(res);})
+  // http.post(wslcb_server, req).success(function(res){console.log(res);})
   // res.send(j);
+  PostCode(login_req)
 });
 
+function PostCode(codestring) {
+  // Build the post string from an object
+  var post_data = querystring.stringify({
+      'compilation_level' : 'ADVANCED_OPTIMIZATIONS',
+      'output_format': 'json',
+      'output_info': 'compiled_code',
+        'warning_level' : 'QUIET',
+        'js_code' : codestring
+  });
+
+  // An object of options to indicate where to post to
+  var post_options = {
+      host: wslcb_server,
+      path: '/serverjson.asp',
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': Buffer.byteLength(post_data)
+      }
+  };
+
+  // Set up the request
+  var post_req = http.request(post_options, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+          console.log('Response: ' + chunk);
+      });
+  });
+
+  // post the data
+  post_req.write(post_data);
+  post_req.end();
+
+}
 
 app.post('/sync_check', function (req, res, next) {
   // res.sendFile('/var/www/potnet.net/index.html')
