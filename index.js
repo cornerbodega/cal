d0135a317efb00ca8905f5c1181b08c00d79dca6
Cornerbodega <merhone@gmail.com>
2016-01-06 20:31:57 -0800

    where do I go?

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ io.on('connection', function(socket){
         console.log('user disconnected');
     });
     socket.on('chat message', function(msg){
-        // console.log('message: ' + msg);
-        to.emit('chat message', msg);
+        console.log('message: ' + msg);
+        // to.emit('chat message', msg);
     });
 });
