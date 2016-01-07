f367dfd49108b98b4c70b2f0ebb8ad75ce2cb50d
Cornerbodega <merhone@gmail.com>
2016-01-07 10:40:59 -0800

    where do I go?

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 var express = require('express')
 var app = express();
+
+
+app.get('/', function (req, res, next) {
+  res.send('Hello World!');
+  res.sendFile('/var/www/potnet.net/index.html')
+});
