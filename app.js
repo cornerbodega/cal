17866b43fedd62e86c11b718c28cff0800e6699f
Cornerbodega <merhone@gmail.com>
2016-01-07 11:19:56 -0800

    where do I go?

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 var express = require('express')
+var proxy = require('express-http-proxy');
 var app = express();
-
+var http = require('http')
 var port = process.env.PORT || 8080;
+
+app.use('/proxy', proxy('www.google.com', {
+  forwardPath: function(req, res) {
+    return require('url').parse(req.url).path;
+  }
+}));
+
+app.get('/sync_check', function (req, res, next) {
+  // res.sendFile('/var/www/potnet.net/index.html')
+  var j = [
+      { name:'Greece', population:'10'},
+      { name: 'Egypt', population:'12'}
+  ]
+
+  // console.log(j);
+  res.send(j);
+});
 
 app.get('/testpath', function (req, res, next) {
   // res.sendFile('/var/www/potnet.net/index.html')
